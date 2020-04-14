d79664684c9e44448f351786dbad249293a3a816
Olakunle Boye <kunlexzy@gmail.com>
2020-04-14 23:24:28 +0100

    made some changes

--- src/Components/Estimation.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-const Estimation = () => {
-    return (
-        <h1>Result</h1>
-    )
-}
-export default Estimation
--- src/Components/MainHeader.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-const MainHeader = () => {
-    return (
-         <h1>Elegant Covid-19 Estimator.</h1>
-    )
-}
-export default MainHeader
